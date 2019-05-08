9767864e5d4bdee8d3441a71797402fddf8a5c0f
Debo borrar mi cuenta de git porque la máquina la usan otros profes <o en todo caso no usar global>
2019-05-07 21:18:46 -0500

    Modulo tres clase Unity 4

--- DiegoHeredia2D/Assets/Scripts/PlayerMovement.cs
+++ DiegoHeredia2D/Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 15;
-
+    public Vector2 limits = new Vector2(5, 3.5f);
+    
+   
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
     // Update is called once per frame
     void Update(){
-        Vector3 horMove = Input.GetAxis("Horizontal") * Vector3.right;
-        Vector3 verMove = Input.GetAxis("Vertical") * Vector3.up;
+        float horDirection = Input.GetAxisRaw("Horizontal");
+        Vector3 horMove = Vector3.zero;
+        if (transform.position.x > -limits.x && transform.position.x < limits.x){
+            horMove = horDirection * Vector3.right;
+            int limit = (int) transform.position.x;
+        }
+
+        float verDirection = Input.GetAxisRaw("Vertical");
+        Vector3 verMove = verDirection * Vector3.up;
 
         transform.Translate((horMove + verMove).normalized * moveSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) { 
+            Debug.Log("Colisiono");
+        }
     }
 }
