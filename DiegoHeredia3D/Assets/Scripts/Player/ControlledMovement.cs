60eb505a6b7b70e61e97bb13e9c49cb92cd6b164
Debo borrar mi cuenta de git porque la máquina la usan otros profes <o en todo caso no usar global>
2019-06-13 22:03:59 -0500

    Modulo tres clase Unity 16

--- DiegoHeredia3D/Assets/Scripts/Player/ControlledMovement.cs
+++ DiegoHeredia3D/Assets/Scripts/Player/ControlledMovement.cs
@@ -9,6 +9,11 @@ public class ControlledMovement : MovScript
     Animator playerAnimator;
     float verticalSpeed;
     public float jumpForce = 10;
+    bool grounded { get { return groundCount > 0; } }
+    int groundCount { get { return groundCollection.Count; } }
+    List<Collider> groundCollection = new List<Collider>();
+    public class Ground { }
+    bool persistence;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +24,9 @@ public class ControlledMovement : MovScript
     // Update is called once per frame
     void Update()
     {
-        RaycastHit info;
-        characterController.Raycast(new Ray(transform.GetChild(0).position, Vector3.down * 5), out info);
-        if (!characterController.isGrounded)
+        //bool grounded = Physics.Raycast(transform.GetChild(0).position, Vector3.down, 0.15f);
+
+        if (grounded)
         {
             verticalSpeed -= gravity * Time.deltaTime;
         }
@@ -32,7 +37,6 @@ public class ControlledMovement : MovScript
             {
                 Debug.Log(verticalSpeed);
                 verticalSpeed = jumpForce;
-                playerAnimator.SetTrigger("Jump");
                 Debug.Log(verticalSpeed);
             }
         }
@@ -40,7 +44,52 @@ public class ControlledMovement : MovScript
         Vector3 verticalAxis = Vector3.up * verticalSpeed;
         Vector3 horizontal = Vector3.up * Input.GetAxis("Horizontal");
 
+        playerAnimator.SetBool("Grounded", grounded);
+
+        Debug.Log(Vector3.Dot(Vector3.up, Vector3.up));
+
         characterController.Move((forwardAxis + verticalAxis) * Time.deltaTime);
         transform.Rotate(horizontal * angularSpeed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collider" + collision.collider.name);
+        Debug.DrawRay(collision.contacts[0].point, collision.contacts[0].normal, Color.red);
+
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            if (Vector3.Dot(collision.contacts[i].normal, Vector3.up) > 0.80)
+            {
+                if (!groundCollection.Contains(collision.collider))
+                {
+                    groundCollection.Add(collision.collider);
+                }
+            }
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        //Ground exitGround = groundCollection.Find(ground => ground.collider == collision.collider);
+        //if (exitGround != null)
+        {
+            //persistence = Physics.Raycast();
+        }
+
+        Debug.Log(persistence);
+        StartCoroutine(ReCheckPersistance());
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.GetChild(0).position, Vector3.down * 0.15f);
+    }
+    
+    IEnumerator ReCheckPersistance()
+    {
+        yield return new WaitForSeconds(0.2f);
+        persistence = groundCount > 0;
     }
 }
